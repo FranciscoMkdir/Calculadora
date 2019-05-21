d2d4a300322d8a50472f952f2ef1f4491797b71f
macbook <macbook@coz3-mac.local>
2019-05-21 09:14:41 -0500

    "First commit"

--- Calculadora/CalculadoraViewController.swift
+++ Calculadora/CalculadoraViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculadoraViewController.swift
 //  Calculadora
 //
 //  Created by macbook on 21/05/19.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculadoraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Calculadora de propinas"
     }
+    
 
+   
 
 }
-
